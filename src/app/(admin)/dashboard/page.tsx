5caa9b345c45113ff0fc1e184cb22e9e22eaba82
QuocHuy <quochuy@phutungb2b.com>
2026-04-20 16:59:04 +0700

    11. Design Login/Register với Next js | Tự Học Fullstack Next.js/Nest.js với TypeScript

---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+import { Layout } from "antd";
+import AdminFooter from "@/components/layout/admin.footer";
+import AdminHeader from "@/components/layout/admin.header";
+import AdminSidebar from "@/components/layout/admin.sidebar";
+
+const DashboardPage = () => {
+  return <>dashboard</>;
+};
+
+export default DashboardPage;
