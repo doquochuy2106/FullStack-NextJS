dfe41844444c9b2e210605aaad918b8783bb3164
QuocHuy <quochuy@phutungb2b.com>
2026-04-26 11:33:15 +0700

    16. Register với Next.js | Tự Học Fullstack Next.js/Nest.js với TypeScript

---
+++ src/app/(guest)/verify/[_id]/page.tsx
@@ -0,0 +1,12 @@
+import Verify from "@/components/auth/verify";
+
+const VerifyPage = ({ params }: { params: { _id: string } }) => {
+  const { _id } = params;
+  return (
+    <>
+      <Verify _id={_id} />
+    </>
+  );
+};
+
+export default VerifyPage;
--- src/components/auth/verify.tsx
+++ src/components/auth/verify.tsx
@@ -1,31 +1,42 @@
 "use client";
 import React from "react";
-import { Button, Col, Divider, Form, Input, notification, Row } from "antd";
+import {
+  Button,
+  Col,
+  Divider,
+  Form,
+  Input,
+  message,
+  notification,
+  Row,
+} from "antd";
 import { ArrowLeftOutlined } from "@ant-design/icons";
 import Link from "next/link";
 import { sendRequest } from "@/utils/api";
 import { useRouter } from "next/navigation";
 
-const Register = () => {
+const Verify = (props: any) => {
+  const { _id } = props;
   const router = useRouter();
+
   const onFinish = async (values: any) => {
     console.log("check value: ", values);
-    const { email, password, name } = values;
+    const { _id, code } = values;
 
     const res = await sendRequest<IBackendRes<any>>({
       method: "POST",
-      url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/register`,
+      url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/check-code`,
       body: {
-        email,
-        password,
-        name,
+        _id,
+        code,
       },
     });
 
     console.log("check res: ", res);
 
     if (res?.data) {
-      router.push(`/verify/${res?.data?._id}`);
+      message.success("Kích hoạt tài khoản thành công");
+      router.push(`/auth/login`);
     } else {
       notification.error({
         message: "Error Register",
@@ -45,41 +56,33 @@ const Register = () => {
             borderRadius: "5px",
           }}
         >
-          <legend>Đăng Ký Tài Khoản</legend>
+          <legend>Kích hoạt tài khoản</legend>
           <Form
             name="basic"
             onFinish={onFinish}
             autoComplete="off"
             layout="vertical"
           >
-            <Form.Item
-              label="Email"
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your email!",
-                },
-              ]}
-            >
-              <Input />
+            <Form.Item label="Id" name="_id" initialValue={_id} hidden>
+              <Input disabled />
             </Form.Item>
+            <div>
+              Mã code đã được ửi tới email đăng ký, vui lòng kiểm tra email
+            </div>
+
+            <Divider />
 
             <Form.Item
-              label="Password"
-              name="password"
+              label="Code"
+              name="code"
               rules={[
                 {
                   required: true,
-                  message: "Please input your password!",
+                  message: "Please input your code!",
                 },
               ]}
             >
               <Input.Password />
-            </Form.Item>
-
-            <Form.Item label="Name" name="name">
-              <Input />
             </Form.Item>
 
             <Form.Item>
@@ -101,4 +104,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Verify;
